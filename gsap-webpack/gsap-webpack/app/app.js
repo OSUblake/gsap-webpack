89a32e35b6fe5865a2367ad828490b964d0a626f
Blake Bowen <blake@blakebowen.com>
2016-09-11 10:17:01 -0400

    Initial

---
+++ gsap-webpack/gsap-webpack/app/app.js
@@ -0,0 +1,22 @@
+﻿require("./css/styles.scss");
+
+import "ScrollToPlugin";
+import Draggable from "Draggable";
+
+Draggable.create("p");
+
+var section1Btn = document.getElementById("section1Btn");
+var section2Btn = document.getElementById("section2Btn");
+var section3Btn = document.getElementById("section3Btn");
+
+section1Btn.onclick = function() {
+  TweenLite.to(window, 1, {scrollTo:{y:"#section1", offsetY:70}});
+}
+
+section2Btn.onclick = function() {
+  TweenLite.to(window, 1, {scrollTo:{y:"#section2", offsetY:70}});
+}
+
+section3Btn.onclick = function() {
+  TweenLite.to(window, 1, {scrollTo:{y:"#section3", offsetY:70}});
+}
---
+++ gsap-webpack/gsap-webpack/webpack.config.js
@@ -0,0 +1,66 @@
+﻿
+var path = require("path");
+var webpack = require("webpack");
+
+var root = __dirname;
+
+var gsapPath = "/node_modules/gsap/src/uncompressed/";
+
+module.exports = {
+  watch: true,
+  context: path.resolve(root, "app"),
+  entry: "./app.js",
+
+  output: {
+    path: "build/js/",
+    publicPath: "/wwwroot/js/",
+    filename: "main.js"
+  },
+
+  devServer: {
+    contentBase: "wwwroot"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: "html-loader"
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: "style-loader!css-loader!autoprefixer-loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: "style-loader!css-loader!autoprefixer-loader!sass-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      TweenMax: "gsap"
+    })
+  ],
+
+  resolve: {
+
+    extensions: ["", ".js"],
+
+    alias: {
+      "TweenLite": path.join(root, gsapPath + "TweenLite.js"),
+      "CSSPlugin": path.join(root, gsapPath + "plugins/CSSPlugin.js"),
+      "Draggable": path.join(root, gsapPath + "utils/Draggable.js"),
+      "ScrollToPlugin": path.join(root, gsapPath + "plugins/ScrollToPlugin.js")
+    }
+  }
+};
